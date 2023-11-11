7b8fd856f46869c59a074013dd07eed2ba85ae3d
Hassna Ouattou <houattou@c1r2p10.1337.ma>
2023-11-11 15:01:31 +0100

    fiing rays problems

--- src/raycasting/draw_player.c
+++ src/raycasting/draw_player.c
@@ -6,7 +6,7 @@
 /*   By: houattou <houattou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 11:44:34 by houattou          #+#    #+#             */
-/*   Updated: 2023/11/10 22:10:35 by houattou         ###   ########.fr       */
+/*   Updated: 2023/11/11 14:59:45 by houattou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,11 +59,11 @@ void initialize_player(t_all_data *data)
          break;
             
     }
-    data->player->y= (float)(i * SIZE_TITLE  +  SIZE_TITLE / 2);
+    data->player->y= (float)(i * SIZE_TITLE  +  SIZE_TITLE / 2) + 1;
     data->player->x =(float) (((ft_strchr(data->map->map[i], direction) - \
-			data->map->map[i])*SIZE_TITLE) + SIZE_TITLE / 2);
-    printf(" x is :%f\n", data->player->x);
-    printf(" y is :%f\n", data->player->y);
+			data->map->map[i])*SIZE_TITLE) + SIZE_TITLE / 2) + 1;
+    // printf(" x is :%f\n", data->player->x);
+    // printf(" y is :%f\n", data->player->y);
 }
 
 
--- src/raycasting/draw_vertical_rays.c
+++ src/raycasting/draw_vertical_rays.c
@@ -6,7 +6,7 @@
 /*   By: houattou <houattou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 11:03:35 by houattou          #+#    #+#             */
-/*   Updated: 2023/11/10 22:36:23 by houattou         ###   ########.fr       */
+/*   Updated: 2023/11/11 14:48:24 by houattou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ void	draw_vertical_intersection(t_all_data *mlx, float ray_angle)
     // printf("vertical angle %f\n", ray_angle);
 
 	ray_angle = normalize_angle(ray_angle);
+	// is_ray_facing_left = ray_angle > M_PI / 2 && ray_angle < 1.5 * M_PI;
 	is_ray_facing_left = ray_angle > M_PI / 2 && ray_angle < 1.5 * M_PI;
 	sign = 1;
 	mlx->cord->xstep_v =ceil(mlx->player->x / SIZE_TITLE) * SIZE_TITLE;
@@ -38,7 +39,7 @@ void	draw_vertical_intersection(t_all_data *mlx, float ray_angle)
 		mlx->player->is_ray_facing_left = FALSE;
 		
 	mlx->cord->ystep_v = mlx->player->y - (mlx->player->x - mlx->cord->xstep_v) * tan(ray_angle);
-	while ((mlx->cord->xstep_v >= 0 && mlx->cord->xstep_v < SIZE_TITLE * mlx->map->width  && mlx->cord->ystep_v >= 0 && mlx->cord->ystep_v < SIZE_TITLE * mlx->map->height ))
+	while (true)
 	{
 		if ((is_ray_facing_left && is_has_wall(mlx, mlx->cord->xstep_v - SIZE_TITLE, mlx->cord->ystep_v))
 		 || (!is_ray_facing_left && is_has_wall(mlx, mlx->cord->xstep_v, mlx->cord->ystep_v)))
