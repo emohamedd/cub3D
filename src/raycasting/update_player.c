6ab979ec9d3e3e4d8cae7cb4ff252a762e33d850
Hassna Ouattou <houattou@c1r5p7.1337.ma>
2023-11-12 10:14:25 +0100

    this is version is well

--- src/raycasting/update_player.c
+++ src/raycasting/update_player.c
@@ -6,7 +6,7 @@
 /*   By: houattou <houattou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 13:43:54 by houattou          #+#    #+#             */
-/*   Updated: 2023/11/10 18:54:38 by houattou         ###   ########.fr       */
+/*   Updated: 2023/11/11 18:01:10 by houattou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,42 +33,64 @@ void ratate_player(mlx_key_data_t keydata, t_all_data *data)
 
 
 
-void update_player(t_all_data *mlx, mlx_key_data_t keydata)
-{
-    float new_x = 0;
-    float new_y = 0;
-    if (keydata.key == MLX_KEY_S)
-    {
-        new_x = mlx->player->x - mlx->player->move_speed * cos(mlx->player->rotation_angle);
-        new_y = mlx->player->y - mlx->player->move_speed * sin(mlx->player->rotation_angle);
-    }
-    else if (keydata.key == MLX_KEY_A)
-    {
-        new_x = mlx->player->x + mlx->player->move_speed * cos(mlx->player->rotation_angle - M_PI / 2);
-        new_y = mlx->player->y + mlx->player->move_speed * sin(mlx->player->rotation_angle - M_PI / 2);
-    }
-    else if (keydata.key == MLX_KEY_D)
-    {
-        new_x = mlx->player->x + mlx->player->move_speed * cos(mlx->player->rotation_angle + M_PI / 2);
-        new_y = mlx->player->y + mlx->player->move_speed * sin(mlx->player->rotation_angle + M_PI / 2);
-    }
-    else if (keydata.key == MLX_KEY_W)
-    {
+// void update_player(t_all_data *mlx, mlx_key_data_t keydata)
+// {
+//     float new_x = 0;
+//     float new_y = 0;
+//     if (keydata.key == MLX_KEY_S)
+//     {
+//         new_x = mlx->player->x - mlx->player->move_speed * cos(mlx->player->rotation_angle);
+//         new_y = mlx->player->y - mlx->player->move_speed * sin(mlx->player->rotation_angle);
+//     }
+//     else if (keydata.key == MLX_KEY_A)
+//     {
+//         new_x = mlx->player->x + mlx->player->move_speed * cos(mlx->player->rotation_angle - M_PI / 2);
+//         new_y = mlx->player->y + mlx->player->move_speed * sin(mlx->player->rotation_angle - M_PI / 2);
+//     }
+//     else if (keydata.key == MLX_KEY_D)
+//     {
+//         new_x = mlx->player->x + mlx->player->move_speed * cos(mlx->player->rotation_angle + M_PI / 2);
+//         new_y = mlx->player->y + mlx->player->move_speed * sin(mlx->player->rotation_angle + M_PI / 2);
+//     }
+//     else if (keydata.key == MLX_KEY_W)
+//     {
        
-        new_x = mlx->player->x + mlx->player->move_speed * cos(mlx->player->rotation_angle);
-        new_y = mlx->player->y + mlx->player->move_speed * sin(mlx->player->rotation_angle);
-    }
+//         new_x = mlx->player->x + mlx->player->move_speed * cos(mlx->player->rotation_angle);
+//         new_y = mlx->player->y + mlx->player->move_speed * sin(mlx->player->rotation_angle);
+//     }
     
-    if( (check_if_have_wall(mlx, new_x, new_y)) == TRUE)
+//     if( (check_if_have_wall(mlx, new_x, new_y)) == TRUE)
+//     {
+  
+//         mlx->player->x = new_x;
+//         mlx->player->y = new_y;
+//     }
+
+// }
+
+void update_player(t_all_data *data, mlx_key_data_t keydata )
+{
+    int sign;
+    float speed;
+    float direction_angle;
+    float new_x;
+    float new_y;
+    
+    sign = 1;
+    direction_angle = data->player->rotation_angle;
+    if (keydata.key == MLX_KEY_S || keydata.key == MLX_KEY_A)
+			sign = -1;
+	if (keydata.key == MLX_KEY_D ||keydata.key == MLX_KEY_A)
+		direction_angle += M_PI / 2;
+    new_x = data->player->x + sign*data->player->move_speed * cos(data->player->rotation_angle);
+    new_y = data->player->y + sign * data->player->move_speed * sin(data->player->rotation_angle); 
+    if( (check_if_have_wall(data, new_x, new_y)) == TRUE)
     {
   
-        mlx->player->x = new_x;
-        mlx->player->y = new_y;
+        data->player->x = new_x;
+        data->player->y = new_y;
     }
-
 }
-
-
 void	my_keyhook(mlx_key_data_t keydata, void *param)
 {
 	t_all_data	*mlx;
